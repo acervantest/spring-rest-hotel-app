01a7b8a7147271faf975c3199a1b161607ddad0f
alejandro <acervantest83@gmail.com>
2021-02-28 19:51:47 -0800

    model objects created

---
+++ src/main/java/com/rest/models/Address.java
@@ -0,0 +1,37 @@
+package com.rest.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue(generator = "add_id", strategy = GenerationType.AUTO)
+    @SequenceGenerator(name = "add_id", sequenceName = "address_id")
+    private Integer addressId;
+    private String city;
+    private long zipcode;
+    private String state;
+
+    public Address(String city, long zipcode, String state) {
+        this.city = city;
+        this.zipcode = zipcode;
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "city='" + city + '\'' +
+                ", zipcode=" + zipcode +
+                ", state='" + state + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/rest/models/Menu.java
@@ -0,0 +1,36 @@
+package com.rest.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Menu {
+
+    @Id
+    @GeneratedValue(generator = "menu_id", strategy = GenerationType.AUTO)
+    @SequenceGenerator(name = "menu_id", sequenceName = "menu_id")
+    private Integer menuId;
+    private String menuName;
+    private double price;
+    @ManyToOne
+    private Hotel hotel;
+
+    public Menu(String menuName, double price) {
+        this.menuName = menuName;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Menu{" +
+                "menuName='" + menuName + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
